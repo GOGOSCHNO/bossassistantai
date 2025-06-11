462971af32be620d16312383e2d63e70d8af04d1
GOGOSCHNO <157826855+GOGOSCHNO@users.noreply.github.com>
2025-06-11 13:53:19 -0500

    Update server.js

--- server.js
+++ server.js
@@ -262,7 +262,7 @@ async function interactWithAssistant(userMessage, userNumber) {
 
     // ▶️ Création d’un nouveau run
     const runResponse = await openai.beta.threads.runs.create(threadId, {
-      assistant_id: "asst_aH2eDHwU2aIIUGjYNAi9h44T"
+      assistant_id: "asst_CWMnVSuxZscjzCB2KngUXn5I"
     });
     const runId = runResponse.id;
     console.log(`▶️ Run lancé : runId = ${runId}`);
